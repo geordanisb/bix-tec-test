3e59248d6cf545c1ad2df6dca034b57d980d2db0
Geordanis B. Vega <gbanoaol@gmail.com>
2025-07-31 21:34:07 -0300

    database on prisma cloud

--- src/app/api/populateTransactionRelation/route.ts
+++ src/app/api/populateTransactionRelation/route.ts
@@ -1,24 +1,27 @@
 
-import { Transaction } from "@/generated/prisma";
-import { prisma } from "@/lib/prisma";
-import { readFile } from "fs";
-import path from "path"
-import { cwd } from "process"
-import { promisify } from "util";
+// import { Transaction } from "@/generated/prisma";
+// import { prisma } from "@/lib/prisma";
+// import { readFile } from "fs";
+// import path from "path"
+// import { cwd } from "process"
+// import { promisify } from "util";
 
-export const dynamic = 'force-static'
+// export const dynamic = 'force-static'
  
+// export async function GET() {
+//   const filepath = path.join(cwd(),'data','transactions.json');
+//   const readFilePromisify = promisify(readFile);
+//   const res = await readFilePromisify(filepath,'utf-8');
+//   const transactions = JSON.parse(res); 
+//   const promises = [];
+  
+//   promises.push(prisma.transaction.createMany({
+//     data: transactions.map((t:Partial<Transaction>)=>({...t,date:t.date!}))
+//   }));
+  
+//   const newtransactions = await Promise.all(promises);
+//   return Response.json({ transactions:newtransactions })
+// }
 export async function GET() {
-  const filepath = path.join(cwd(),'data','transactions.json');
-  const readFilePromisify = promisify(readFile);
-  const res = await readFilePromisify(filepath,'utf-8');
-  const transactions = JSON.parse(res); 
-  const promises = [];
-  
-  promises.push(prisma.transaction.createMany({
-    data: transactions.map((t:Partial<Transaction>)=>({...t,date:t.date!}))
-  }));
-  
-  const newtransactions = await Promise.all(promises);
-  return Response.json({ transactions:newtransactions })
+  return Response.json({ ok:1 })
 }
